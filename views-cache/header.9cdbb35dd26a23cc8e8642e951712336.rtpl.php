cebaeb80718bfe270fc0473740403ef68e5172fb
Thiago <thmoura14@hotmail.com>
2018-06-17 20:40:30 -0300

    removendo o procfile

---
+++ views-cache/header.9cdbb35dd26a23cc8e8642e951712336.rtpl.php
@@ -0,0 +1,57 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta http-equiv="refresh" content="2000;URL=/admin/ausente">
+
+  <title>Admin | Jayme da Fonte</title>
+
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="/res/admin/plugins/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="/res/admin/dist/css/adminlte.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="/res/admin/plugins/datatables/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.css">
+
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <!-- Link do favicon -->
+  <link rel="shortcut icon" href="/res/admin/dist/img/favicon.ico" type="image/x-icon">
+
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="/res/admin/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link rel="stylesheet" href="/res/admin/plugins/iCheck/all.css">
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <script src="https://cdn.ckeditor.com/ckeditor5/10.0.1/classic/ckeditor.js"></script>
+
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+      </li>      
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+    <a href="/admin/logout">
+      <i class="fa fa-sign-out"></i> Sair
+    </a>
+    </ul>
+
+  </nav>
+  <!-- /.navbar -->
