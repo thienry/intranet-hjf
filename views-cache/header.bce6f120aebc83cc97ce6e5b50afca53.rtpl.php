af69ba62741088a028ae972404319d0d69e3f23e
Thiago <thmoura14@hotmail.com>
2018-06-24 20:02:34 -0300

    views cache

---
+++ views-cache/header.bce6f120aebc83cc97ce6e5b50afca53.rtpl.php
@@ -0,0 +1,36 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Intranet - Jayme da fonte</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="/res/site/css/bootstrap.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="/res/site/css/mdb.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="/res/site/css/mdb2.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="/res/site/css/style.css" rel="stylesheet">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="/res/admin/plugins/datatables/dataTables.bootstrap4.min.css">
+    <!-- Ownl Carousel-->
+    <link rel="stylesheet" href="/res/site/OwlCarousel2-2.3.4/dist/assets/owl.carousel.css">
+    <!-- Link do favicon -->
+    <link rel="shortcut icon" href="/res/admin/dist/img/favicon.ico" type="image/x-icon">
+    <!-- DateDropper -->
+    <link rel="stylesheet" href="/res/site/css/datedropper.css">
+    
+    <style type="text/css">
+      @media (min-width: 800px) and (max-width: 850px) {
+              .navbar:not(.top-nav-collapse) {
+                  background: #8d1d2e!important;
+              }
+          }
+    </style>
+</head>
+
